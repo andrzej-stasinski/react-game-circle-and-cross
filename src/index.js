8dee56a737d1aef9e019d803e23988be2b4e8293
Andrzej Stasinski <andis_as@wp.pl>
2021-02-25 18:44:29 +0100

    one square works on click

--- src/index.js
+++ src/index.js
@@ -2,35 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Board2 extends React.Component {
+class Square extends React.Component {
     render() {
         return(
-            <h2>Hello World {this.props.name}</h2>
+            <div 
+                onClick={this.props.onClick} 
+                className="Square">
+                {this.props.value}
+            </div>
         ); 
     }
 }
 
-function Board1(props) {
-    return <h2>Hello {props.name}</h2>
+class Board extends React.Component {
+    state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+    }
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        console.log(squares);
+        squares[i] = this.state.xIsNext ? 'X' : 'Y';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
+    }
+    render() {
+        return(
+            <div>
+                <div className="board-row">
+                <Square 
+                    onClick={() => this.handleClick(0)} 
+                    value={this.state.squares[0]}
+                />
+                </div>
+            </div>            
+        );
+    }
 }
-
-const Board3 = (props) => {
-    return <h2>Hello {props.name}</h2>
-}
-
-const Board4 = (props) => (
-    <h2>Hello {props.name}</h2>
-)
 
 class Game extends React.Component {
     render() {
         return(
             <div>
-                <h1>Przekazanie atrybutów jako props</h1>
-                <Board1 name="Antoni nr 1"/>
-                <Board2 name="Babra nr 2"/>
-                <Board3 name="Genowefa nr 3"/>
-                <Board4 name="Gerwazy nr 4"/>
+                <Board/>
             </div>
         );
     }
