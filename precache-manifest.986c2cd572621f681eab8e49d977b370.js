5c1aedb99f1774716674994964feac855610ca48
Andrzej Stasinski <andis_as@wp.pl>
2021-03-17 16:31:57 +0100

    Updates

---
+++ precache-manifest.986c2cd572621f681eab8e49d977b370.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86327f6a671576d473cf70784126cc28",
+    "url": "/react-game-circle-and-cross/index.html"
+  },
+  {
+    "revision": "103a6c52b8501f81a2c4",
+    "url": "/react-game-circle-and-cross/static/css/main.3f1b6b09.chunk.css"
+  },
+  {
+    "revision": "14c67cde07a6c29701fe",
+    "url": "/react-game-circle-and-cross/static/js/2.0b352f91.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-game-circle-and-cross/static/js/2.0b352f91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "103a6c52b8501f81a2c4",
+    "url": "/react-game-circle-and-cross/static/js/main.3da486ed.chunk.js"
+  },
+  {
+    "revision": "8fdc8c0cf30ca110c4c6",
+    "url": "/react-game-circle-and-cross/static/js/runtime-main.961302c8.js"
+  },
+  {
+    "revision": "7205563cbfae6e76c375cfeb0cc5b09f",
+    "url": "/react-game-circle-and-cross/static/media/bum.7205563c.mp3"
+  },
+  {
+    "revision": "af807402fc25e427240299444fd4c249",
+    "url": "/react-game-circle-and-cross/static/media/end.af807402.mp3"
+  },
+  {
+    "revision": "ebda6c040d450db7bc0b9d4e2108bf42",
+    "url": "/react-game-circle-and-cross/static/media/plam.ebda6c04.wav"
+  },
+  {
+    "revision": "5baac27f1347318cb9faf86662fe70c3",
+    "url": "/react-game-circle-and-cross/static/media/plum.5baac27f.mp3"
+  },
+  {
+    "revision": "5b88b3032fd68c3f7a8ce28b10324e9f",
+    "url": "/react-game-circle-and-cross/static/media/win.5b88b303.wav"
+  }
+]);
